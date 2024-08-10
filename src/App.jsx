c1734b44b363c24ea96a5d71a5ad29fd0e1aa73a
artog1809 <gdoq17@mail.ru>
2024-08-10 14:25:26 +0300

    [feat]: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './app.module.css'
+
+function App() {
+
+  return (
+    <div className={styles.app}>
+      <p>hi</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/cmp/header/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./heder.module.css"
+
+const Header = () => {
+
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.left}></div>            
+        </div>
+    )
+}
+
+export default Header;
